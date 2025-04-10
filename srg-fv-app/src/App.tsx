8d39ddd356f6713bcffbc1fd08a3c23222e25131
Max <max.meier.13@live.de>
2025-04-10 17:21:54 +0200

    update logo, cleanup

--- srg-fv-app/src/App.tsx
+++ srg-fv-app/src/App.tsx
@@ -1,4 +1,4 @@
-function App() {
+export default function App() {
 
   return (
     <>
@@ -6,5 +6,3 @@ function App() {
     </>
   )
 }
-
-export default App
--- srg-fv-app/src/main.tsx
+++ srg-fv-app/src/main.tsx
@@ -1,12 +1,14 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
 import { BrowserRouter, Routes, Route } from "react-router";
+import Apply from './features/apply/Apply.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
+    </Routes><Routes>
+      <Route path="/antrag" element={<Apply />} />
     </Routes>
-  </BrowserRouter>,
+  </BrowserRouter>
 )
