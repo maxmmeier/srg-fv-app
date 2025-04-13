9f4e5aad23bb69a0b90e0de7e0c7c4311404759b
Max <max.meier.13@live.de>
2025-04-13 09:49:58 +0200

    add form to apply membership page
    realtewd work item: #1

--- srg-fv-app/src/features/apply/Apply.tsx
+++ srg-fv-app/src/features/apply/Apply.tsx
@@ -1,10 +1,92 @@
+import { Button, Col, Form, InputGroup, Row } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 
 export function Apply() {
   const { t } = useTranslation();
   return (
     <>
-      <div>{t("applyForMembership")}</div>
+      <Form>
+        <Row>
+          <div className="mb-3">{t("applyForMembershipIntro")}</div>
+        </Row>
+
+        <Row xs={1} sm={1} md={2}>
+          <Col>
+            <Form.Group className="mb-3">
+              <Form.Label>{t("lastname")}</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder={t("lastnamePlaceholder")}
+                required
+              ></Form.Control>
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group className="mb-3">
+              <Form.Label>{t("firstname")}</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder={t("firstnamePlaceholder")}
+                required
+              ></Form.Control>
+            </Form.Group>
+          </Col>
+        </Row>
+
+        <Row xs={1} sm={1} md={2}>
+          <Col>
+            <Form.Group className="mb-3">
+              <Form.Label>{t("email")}</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder={t("emailPlaceholder")}
+                required
+              ></Form.Control>
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group className="mb-3">
+              <Form.Label>{t("dateOfBirth")}</Form.Label>
+              <Form.Control type="date" required></Form.Control>
+            </Form.Group>
+          </Col>
+        </Row>
+
+        <Row xs={1} sm={1} md={2}>
+          <Col>
+            {" "}
+            <Form.Group className="mb-3">
+              <Form.Label>{t("street")}</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder={t("streetPlaceholder")}
+                required
+              ></Form.Control>
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group className="mb-3">
+              <Form.Label>{t("zipAndCity")}</Form.Label>
+              <InputGroup>
+                <Form.Control
+                  type="number"
+                  placeholder={t("zipPlaceholder")}
+                  required
+                ></Form.Control>
+                <Form.Control
+                  type="text"
+                  placeholder={t("cityPlaceholder")}
+                  required
+                ></Form.Control>
+              </InputGroup>
+            </Form.Group>
+          </Col>
+        </Row>
+
+        <Button variant="primary" type="submit" className="float-end">
+          {t("submit")}
+        </Button>
+      </Form>
     </>
   );
 }
--- srg-fv-app/src/i18n.ts
+++ srg-fv-app/src/i18n.ts
@@ -14,7 +14,20 @@ i18n
         translation: {
             "applyForMembership" : "Mitgliedsantrag",
             "supportAssociationSrgStuttgart" : "Förderverein SRG Stuttgart",
-
+            "applyForMembershipIntro" : "Hiermit erkläre ich meinen Beitritt zum Förderverein der Schiedsrichter-Gruppe Stuttgart e.V.\nDer Mitgliedsbeitrag beträgt 30,- EUR pro Jahr. Bei Minderjährigen ist die Unterschrift des/der Erziehungsberechtigen erforderlich.",
+            "lastname" : "Name",
+            "lastnamePlaceholder" : "Bitte Nachname eintragen",
+            "firstname" : "Vorname",
+            "firstnamePlaceholder" : "Bitte Vorname eintragen",
+            "dateOfBirth" : "Geburtsdatum",
+            "email" : "E-Mail",
+            "emailPlaceholder" : "Bitte E-Mail Adresse eintragen",
+            "street" : "Straße",
+            "streetPlaceholder" : "Bitte Straße eintragen",
+            "zipAndCity" : "PLZ / Ort",
+            "zipPlaceholder" : "Bitte PLZ eintragen",
+            "cityPlaceholder" : "Bitte Ort eintragen",
+            "submit" : "Mitgliedschaft beantragen",
         }
       }
     }
