921d666830ed1adce375cee1f72c1b8b950e7d7b
Max <max.meier.13@live.de>
2025-04-15 15:42:31 +0200

    get values from from
    related work item #1

--- srg-fv-app/src/features/apply/Apply.tsx
+++ srg-fv-app/src/features/apply/Apply.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { MouseEvent, useRef, useState } from "react";
 import { Button, Col, Form, InputGroup, Row } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import SignatureCanvas from "react-signature-canvas";
@@ -8,17 +8,66 @@ import "./Apply.css";
 import { formatIban } from "./Iban";
 
 export function Apply() {
+  const [lastName, setLastName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [dateOfBirth, setDateOfBirth] = useState("");
+  const [street, setStreet] = useState("");
+  const [zip, setZip] = useState("");
+  const [city, setCity] = useState("");
   const [isMemberNotAccountHolder, setIsMemberNotAccountHolder] =
     useState(false);
+  const [lastNameSepa, setLastNameSepa] = useState("");
+  const [firstNameSepa, setFirstNameSepa] = useState("");
+  const [streetSepa, setStreetSepa] = useState("");
+  const [zipSepa, setZipSepa] = useState("");
+  const [citySepa, setCitySepa] = useState("");
+  const [bank, setBank] = useState("");
+  const [bic, setBic] = useState("");
   const [iban, setIban] = useState("");
+  const [mandate, setMandate] = useState("");
 
   const canvasWrapperMember = useRef(null);
   const canvasWrapperSepa = useRef(null);
-
   const memberSignature = useContainerDimensions(canvasWrapperMember);
   const sepaSignature = useContainerDimensions(canvasWrapperSepa);
+  const memberSignatureCanvas = useRef<SignatureCanvas>(null);
+  const sepaSignatureCanvas = useRef<SignatureCanvas>(null);
 
   const { t } = useTranslation();
+
+  function onSubmit(e: MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+
+    var result = {
+      lastName,
+      firstName,
+      email,
+      dateOfBirth,
+      street,
+      zip,
+      city,
+      memberSignature: memberSignatureCanvas.current
+        ?.getCanvas()
+        .toDataURL("image/png"),
+      isMemberNotAccountHolder,
+      lastNameSepa,
+      firstNameSepa,
+      streetSepa,
+      zipSepa,
+      citySepa,
+      bank,
+      bic,
+      iban,
+      mandate,
+      sepaSignature: sepaSignatureCanvas.current
+        ?.getCanvas()
+        .toDataURL("image/png"),
+    };
+
+    console.log(result);
+  }
+
   return (
     <>
       <Form>
@@ -31,6 +80,8 @@ export function Apply() {
             <Form.Group className="mb-3">
               <Form.Label>{t("lastname")}</Form.Label>
               <Form.Control
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
                 type="text"
                 placeholder={t("lastnamePlaceholder")}
                 required
@@ -41,6 +92,8 @@ export function Apply() {
             <Form.Group className="mb-3">
               <Form.Label>{t("firstname")}</Form.Label>
               <Form.Control
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
                 type="text"
                 placeholder={t("firstnamePlaceholder")}
                 required
@@ -54,6 +107,8 @@ export function Apply() {
             <Form.Group className="mb-3">
               <Form.Label>{t("email")}</Form.Label>
               <Form.Control
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 type="text"
                 placeholder={t("emailPlaceholder")}
                 required
@@ -63,7 +118,12 @@ export function Apply() {
           <Col>
             <Form.Group className="mb-3">
               <Form.Label>{t("dateOfBirth")}</Form.Label>
-              <Form.Control type="date" required></Form.Control>
+              <Form.Control
+                value={dateOfBirth}
+                onChange={(e) => setDateOfBirth(e.target.value)}
+                type="date"
+                required
+              ></Form.Control>
             </Form.Group>
           </Col>
         </Row>
@@ -74,6 +134,8 @@ export function Apply() {
             <Form.Group className="mb-3">
               <Form.Label>{t("street")}</Form.Label>
               <Form.Control
+                value={street}
+                onChange={(e) => setStreet(e.target.value)}
                 type="text"
                 placeholder={t("streetPlaceholder")}
                 required
@@ -85,11 +147,15 @@ export function Apply() {
               <Form.Label>{t("zipAndCity")}</Form.Label>
               <InputGroup>
                 <Form.Control
+                  value={zip}
+                  onChange={(e) => setZip(e.target.value)}
                   type="number"
                   placeholder={t("zipPlaceholder")}
                   required
                 ></Form.Control>
                 <Form.Control
+                  value={city}
+                  onChange={(e) => setCity(e.target.value)}
                   type="text"
                   placeholder={t("cityPlaceholder")}
                   required
@@ -105,6 +171,7 @@ export function Apply() {
             <div className="input-group">
               <div className="form-control signature" ref={canvasWrapperMember}>
                 <SignatureCanvas
+                  ref={memberSignatureCanvas}
                   canvasProps={{
                     width: memberSignature.width,
                     height: memberSignature.height,
@@ -149,6 +216,8 @@ export function Apply() {
                 <Form.Group className="mb-3">
                   <Form.Label>{t("lastname")}</Form.Label>
                   <Form.Control
+                    value={lastNameSepa}
+                    onChange={(e) => setLastNameSepa(e.target.value)}
                     type="text"
                     placeholder={t("lastnamePlaceholder")}
                     required
@@ -159,6 +228,8 @@ export function Apply() {
                 <Form.Group className="mb-3">
                   <Form.Label>{t("firstname")}</Form.Label>
                   <Form.Control
+                    value={firstNameSepa}
+                    onChange={(e) => setFirstNameSepa(e.target.value)}
                     type="text"
                     placeholder={t("firstnamePlaceholder")}
                     required
@@ -173,6 +244,8 @@ export function Apply() {
                 <Form.Group className="mb-3">
                   <Form.Label>{t("street")}</Form.Label>
                   <Form.Control
+                    value={streetSepa}
+                    onChange={(e) => setStreetSepa(e.target.value)}
                     type="text"
                     placeholder={t("streetPlaceholder")}
                     required
@@ -184,11 +257,15 @@ export function Apply() {
                   <Form.Label>{t("zipAndCity")}</Form.Label>
                   <InputGroup>
                     <Form.Control
+                      value={zipSepa}
+                      onChange={(e) => setZipSepa(e.target.value)}
                       type="number"
                       placeholder={t("zipPlaceholder")}
                       required
                     ></Form.Control>
                     <Form.Control
+                      value={citySepa}
+                      onChange={(e) => setCitySepa(e.target.value)}
                       type="text"
                       placeholder={t("cityPlaceholder")}
                       required
@@ -207,6 +284,8 @@ export function Apply() {
             <Form.Group className="mb-3">
               <Form.Label>{t("bank")}</Form.Label>
               <Form.Control
+                value={bank}
+                onChange={(e) => setBank(e.target.value)}
                 type="text"
                 placeholder={t("bankPlaceholder")}
                 required
@@ -217,6 +296,8 @@ export function Apply() {
             <Form.Group className="mb-3">
               <Form.Label>{t("bic")}</Form.Label>
               <Form.Control
+                value={bic}
+                onChange={(e) => setBic(e.target.value)}
                 type="text"
                 placeholder={t("bicPlaceholder")}
                 required
@@ -244,6 +325,8 @@ export function Apply() {
             <Form.Group className="mb-3">
               <Form.Label>{t("mandate")}</Form.Label>
               <Form.Control
+                value={mandate}
+                onChange={(e) => setMandate(e.target.value)}
                 type="text"
                 placeholder={t("mandatePlaceholder")}
                 required
@@ -258,6 +341,7 @@ export function Apply() {
             <div className="input-group">
               <div className="form-control signature" ref={canvasWrapperSepa}>
                 <SignatureCanvas
+                  ref={sepaSignatureCanvas}
                   canvasProps={{
                     width: sepaSignature.width,
                     height: sepaSignature.height,
@@ -268,7 +352,12 @@ export function Apply() {
           </Form.Group>
         </Row>
 
-        <Button variant="primary" type="submit" className="float-end">
+        <Button
+          variant="primary"
+          type="submit"
+          className="float-end"
+          onClick={(e) => onSubmit(e)}
+        >
           {t("submit")}
         </Button>
       </Form>
